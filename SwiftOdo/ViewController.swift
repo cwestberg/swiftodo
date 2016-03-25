2891d570505ce2ef1f95077895480d1c91546902
Clarence Westberg <clarence.westberg@gmail.com>
2016-03-25 10:01:36 -0500

    Constraints and orientation finally figured out.

--- SwiftOdo/ViewController.swift
+++ SwiftOdo/ViewController.swift
@@ -60,7 +60,13 @@ class ViewController: UIViewController,UITableViewDelegate, UITableViewDataSourc
 
     }
     
+    override func viewWillAppear(animated: Bool) {
+        
+        super.viewWillAppear(animated)
+        tableView.reloadData()
 
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
